6f1d57c5937e50c7f699c27db09bcda5e6743a5c
siladolas <147716884+siladolas@users.noreply.github.com>
2024-12-17 21:54:58 +0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -6,6 +6,7 @@ void display_help() {
     printf("  look          - Look around the current room.\n");
     printf("  move <dir>    - Move to another room (directions: up, down, left, right).\n");
     printf("  inventory     - Show your inventory.\n");
+    printf("  use <item>    - Use an item on the inventory.\n");
     printf("  pickup <item> - Pick up an item from the current room.\n");
     printf("  drop <item>   - Drop an item from the inventory.\n");
     printf("  attack        - Attack a creature in the current room.\n");
